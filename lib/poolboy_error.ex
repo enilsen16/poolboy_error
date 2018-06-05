1442e76a4b5336ac97c63a832dc37ea1ef48bb52
Erik Nilsen <enilsen16@live.com>
2018-06-05 22:04:56 +0200

    initial commit

---
+++ lib/poolboy_error.ex
@@ -0,0 +1,18 @@
+defmodule PoolboyError do
+  @moduledoc """
+  Documentation for PoolboyError.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PoolboyError.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poolboy_error_test.exs
@@ -0,0 +1,8 @@
+defmodule PoolboyErrorTest do
+  use ExUnit.Case
+  doctest PoolboyError
+
+  test "greets the world" do
+    assert PoolboyError.hello() == :world
+  end
+end
